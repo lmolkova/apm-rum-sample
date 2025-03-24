52a417966f31b55c4013755387795bac06c5585e
Liudmila Molkova <limolkova@microsoft.com>
2025-03-23 20:05:55 -0700

    instrument demo app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,16 @@ import "./App.css";
 import { PlaceOrderPage } from "./components/PlaceOrderPage";
 import { ProductsPage } from "./components/ProductsPage";
 import { WelcomePage } from "./components/WelcomePage";
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 
 import { createRoutesFromChildren, matchRoutes, Routes, useLocation, useNavigationType } from "react-router-dom";
+import {
+  initializeFaro as coreInit,
+  getWebInstrumentations,
+  ReactIntegration,
+  ReactRouterVersion,
+  FaroRoutes
+} from '@grafana/faro-react';
 
 function App() {
   return (
@@ -25,14 +33,57 @@ function App() {
             </li>
           </ul>
         </div>
-        <Routes>
+        <FaroRoutes>
           <Route exact path="/" element={<WelcomePage />} />
           <Route exact path="products" element={<ProductsPage />} />
           <Route exact path="order" element={<PlaceOrderPage />} />
-        </Routes>
+        </FaroRoutes>
       </Router>
     </div>
   );
+}
+
+export function initializeFaro() {
+
+  const faro = coreInit({
+    // This does some magic that works along with "Frontend" in the Grafana Cloud UI
+    // it also avoids CORS issues when running locally
+    url: "https://faro-collector-prod-us-central-0.grafana.net/collect/14c8f20a36a0c4f7b267cf568ce88385",
+
+    // This sends to local alloy instance, and will send traces and logs to the
+    // Grafana Cloud, but they won't show up in the "Frontend" UI, but will be
+    // available in the traces and logs sources.
+    // url: `http://localhost:12347/collect`,
+    apiKey: "api_key",
+    trackWebVitalsAttribution: true,
+    instrumentations: [
+      ...getWebInstrumentations({
+        captureConsole: true,
+      }),
+      new TracingInstrumentation(),
+      new ReactIntegration({
+        router: {
+          version: ReactRouterVersion.V6,
+          dependencies: {
+            createRoutesFromChildren,
+            matchRoutes,
+            Routes,
+            useLocation,
+            useNavigationType,
+          },
+        },
+      }),
+    ],
+    app: {
+      name: "frontend",
+      version: '1.0.0',
+      environment: 'production'
+    }
+  });
+
+  faro.api.pushLog(['Faro was initialized']);
+
+  return faro;
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import { initializeFaro } from "./App"
 import "./index.css";
+
+initializeFaro();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
